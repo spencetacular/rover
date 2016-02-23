6847c7c8b3ff7ecfd6ede0e53fb6a02003d2ab2a
Spencer Christopherson <schristopherson@gmail.com>
2016-02-23 16:35:27 -0700

    first commit

---
+++ Scripts/RoverLogic.js
@@ -0,0 +1,69 @@
+﻿#pragma strict
+
+public var roverMovement : RoverMovement;
+public var bottomCamera : GameObject;
+
+var stopDelayTicker : float;
+var photographDelayTicker : float;
+var stopDelayDuration : float;
+var photographDelayDuration : float;
+var mode = "detecting";
+
+function Start () {
+	roverMovement = GetComponent(RoverMovement);
+
+	stopDelayDuration = 0.8;
+	photographDelayDuration = 1.0;
+	stopDelayTicker = stopDelayDuration;
+	photographDelayTicker = photographDelayDuration;
+
+}
+
+function Update () {
+
+	if(mode == "stopping"){
+		Stop();
+	}
+	if(mode === "photographing"){
+		Photograph();
+	}
+
+//	Debug.Log(roverMovement.paused);
+//	Debug.Log(mode);
+
+}
+
+function Stop(){
+	stopDelayTicker -= Time.deltaTime;
+	if(stopDelayTicker <= 0){
+		roverMovement.paused = true;
+		mode = "photographing";
+	}
+}
+
+function Photograph(){
+	photographDelayTicker -= Time.deltaTime;
+	bottomCamera.GetComponent(Light).intensity = 0.3;
+
+	if(photographDelayTicker <= 0){
+		roverMovement.paused = false;
+		mode = "detecting";
+		stopDelayTicker = stopDelayDuration;
+		photographDelayTicker = stopDelayDuration;
+		bottomCamera.GetComponent(Light).intensity = 0.0;
+
+	}
+}
+
+function OnTriggerEnter (other: Collider)
+{
+	Debug.Log("Collision");
+	other.gameObject.GetComponent(BoxCollider).enabled = false;
+	
+	if(mode == "detecting" && roverMovement.mode == "moveLong"){
+		mode = "stopping";
+		Debug.Log("called");
+	}
+
+}
+
---
+++ Scripts/RoverMovement.js
@@ -0,0 +1,145 @@
+﻿#pragma strict
+
+var movementSpeed = 1.0;                
+var turnSpeed = 30.0;          
+
+var moveLongDuration : float;
+var moveLongTicker : float;
+var moveShortDuration : float;
+var moveShortTicker : float;
+
+var turnedRight :boolean;
+var paused = false;
+var mode = "moveLong";
+private var m_Rigidbody: Rigidbody;
+
+function Awake() {
+	m_Rigidbody = GetComponent(Rigidbody);
+}
+
+function Start () {
+	moveLongDuration = 5.0;
+	moveLongTicker = moveLongDuration;
+	moveShortDuration = 0.5;
+	moveShortTicker =moveShortDuration;
+	turnedRight = false;
+
+}
+
+function Update () {
+
+	if(paused == false){
+		if(mode == "moveLong"){
+		MoveForwardLong();
+		}
+		if(mode == "firstTurnRight"){
+			TurnRightFirst();
+		}
+		if(mode == "moveShort"){
+			MoveForwardShort();
+		}
+		if(mode == "secondTurnRight"){
+			TurnRightSecond();
+		}
+		if(mode == "firstTurnLeft"){
+			TurnLeftFirst();
+		}
+		if(mode == "secondTurnLeft"){
+			TurnLeftSecond();
+		}
+
+//		Debug.Log("Ticker "+ moveLongTicker);
+//		Debug.Log("Short Ticker "+ moveShortTicker);
+//		Debug.Log(mode);
+	}
+}
+
+private function MoveForwardLong() {
+
+	moveLongTicker -= Time.deltaTime;
+
+	 if (moveLongTicker >= 0 ) {
+		var movement: Vector3 = transform.forward * movementSpeed * Time.deltaTime;
+		m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+
+	 } 
+	 else{
+		if(turnedRight == false){
+			mode = "firstTurnRight";	
+		}else{
+			mode = "firstTurnLeft";
+		}
+	 }
+}
+
+private function MoveForwardShort() {
+
+	 if (moveShortTicker >= 0 ) {
+	 	moveShortTicker -= Time.deltaTime;
+		var movement: Vector3 = transform.forward * movementSpeed * Time.deltaTime;
+		m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+
+	 } else{
+	 	if(turnedRight == false){
+			mode = "secondTurnRight";	
+		}else{
+			mode = "secondTurnLeft";
+		}
+	 }
+}
+
+private function TurnRightFirst() {
+
+	if(transform.eulerAngles.y < 90.0){
+		transform.Rotate(Vector3.up,  turnSpeed * Time.deltaTime);
+
+	}else{
+		mode = "moveShort";
+	}
+}
+
+private function TurnRightSecond() {
+
+	if(transform.eulerAngles.y < 180.0){
+		transform.Rotate(Vector3.up,  turnSpeed * Time.deltaTime);
+
+	}else{
+		mode = "moveLong";
+		moveLongTicker = moveLongDuration;
+		moveShortTicker = moveShortDuration;
+		turnedRight = true;
+	}
+}
+
+private function TurnLeftFirst() {
+
+	if(transform.eulerAngles.y > 90.0){
+		transform.Rotate(Vector3.up,  -turnSpeed * Time.deltaTime);
+	}else{
+		mode = "moveShort";
+	}
+}
+
+private function TurnLeftSecond() {
+
+	if(transform.eulerAngles.y > 1.0  ){
+		transform.Rotate(Vector3.up,  -turnSpeed * Time.deltaTime);
+
+	}else{
+		mode = "moveLong";
+		moveLongTicker = moveLongDuration;
+		moveShortTicker = moveShortDuration;
+		turnedRight = false;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
